2173c909064b4f6363bec79573219ca909b5e041
Bhargav Panth <bhargavrpanth@gmail.com>
2020-03-20 21:42:02 +0000

    adding script to download and extract dataset

--- download_dataset.py
+++ download_dataset.py
@@ -1,26 +1,30 @@
 '''
 Downloading and extracting the MovieLense dataset
 '''
+import zipfile
 import urllib.request
 
 def download_dataset():
     # Downloading movie lense dataset
     url = 'http://files.grouplens.org/datasets/movielens/ml-100k.zip'
-    urllib.request.urlretrieve(url, 'dataset')
+    urllib.request.urlretrieve(url, 'dataset.zip')
 
 def download_checksum():
     # Downloading dataset checksum
     checksum_url = 'http://files.grouplens.org/datasets/movielens/ml-100k.zip.md5'
-    urllib.request.urlretrieve(checksum_url, 'checksum')
+    urllib.request.urlretrieve(checksum_url, 'checksum.md5')
 
 def extract_dataset():
-    pass
-
+    with zipfile.ZipFile('./dataset.zip', 'r') as zip_ref:
+        zip_ref.extractall('.')
 
 def delete_zipped_dataset_folder():
     pass
 
 def main():
+    download_dataset()
+    download_checksum()
+    extract_dataset()
     # download dataset
     # download checksum
     # extract dataset
