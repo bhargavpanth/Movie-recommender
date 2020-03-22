487d0661c57747065a364dc1c70c26d8acd7babe
Bhargav Panth <bhargavrpanth@gmail.com>
2020-03-22 00:41:23 +0000

    added remove datasets script

--- download_dataset.py
+++ download_dataset.py
@@ -3,33 +3,29 @@ Downloading and extracting the MovieLense dataset
 '''
 import zipfile
 import urllib.request
+import os
 
 def download_dataset():
     # Downloading movie lense dataset
     url = 'http://files.grouplens.org/datasets/movielens/ml-100k.zip'
     urllib.request.urlretrieve(url, 'dataset.zip')
 
-def download_checksum():
-    # Downloading dataset checksum
-    checksum_url = 'http://files.grouplens.org/datasets/movielens/ml-100k.zip.md5'
-    urllib.request.urlretrieve(checksum_url, 'checksum.md5')
 
 def extract_dataset():
     with zipfile.ZipFile('./dataset.zip', 'r') as zip_ref:
         zip_ref.extractall('.')
 
 def delete_zipped_dataset_folder():
-    pass
+    os.unlink('./dataset.zip')
 
 def main():
-    download_dataset()
-    download_checksum()
-    extract_dataset()
     # download dataset
-    # download checksum
+    download_dataset()
     # extract dataset
-    # read contents from checksum file and check the checksum of the extracted dataset
-    pass
+    extract_dataset()
+    # delete the zip files
+    delete_zipped_dataset_folder()
+    print('Dataset ready to be used')
 
 if __name__ == '__main__':
     main()
