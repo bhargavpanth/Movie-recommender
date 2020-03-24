59d12f05f48818cd29c8f6159d96ca3c8c4ffc19
Bhargav Panth <bhargavrpanth@gmail.com>
2020-03-24 00:48:48 +0000

    adding movie recommendation class

--- movie-recommender.py
+++ movie-recommender.py
@@ -1,3 +1,8 @@
-import sys
-from pyspark import SparkConf, SparkContext
-from math import sqrt
+class MovieRecommender:
+    def __init__(self):
+        self.dataset_folder = './ml-100k/'
+        self.item = self.dataset_folder + 'u.item'
+        self.data = self.dataset_folder + 'u.data'
+
+    def load_movie_names(self):
+        pass
