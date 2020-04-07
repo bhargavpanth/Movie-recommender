632884f67602382182b34414b878da3f9e1fe20f
Bhargav Panth <bhargavrpanth@gmail.com>
2020-04-07 01:50:18 +0000

    adding popular movie class

--- popular_movie.py
+++ popular_movie.py
@@ -3,10 +3,15 @@ from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, IntegerType, LongType
 import codecs
 
-def loadMovieNames():
+def load_movie_names():
     movieNames = {}
     with codecs.open('./ml-100k/u.ITEM', 'r', encoding='ISO-8859-1', errors='ignore') as f:
         for line in f:
             fields = line.split('|')
             movieNames[int(fields[0])] = fields[1]
     return movieNames
+
+class PopularMovie:
+    def __init__(self):
+        pass
+
