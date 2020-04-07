f5f23472405c9d821f6e0bd8eb33ca90181a36a0
Bhargav Panth <bhargavrpanth@gmail.com>
2020-04-07 01:38:34 +0000

    loading movie names

--- popular_movie.py
+++ popular_movie.py
@@ -3,3 +3,10 @@ from pyspark.sql import functions as func
 from pyspark.sql.types import StructType, StructField, IntegerType, LongType
 import codecs
 
+def loadMovieNames():
+    movieNames = {}
+    with codecs.open('./ml-100k/u.ITEM', 'r', encoding='ISO-8859-1', errors='ignore') as f:
+        for line in f:
+            fields = line.split('|')
+            movieNames[int(fields[0])] = fields[1]
+    return movieNames
